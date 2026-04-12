c9c8023f2cb4b2ba705b15341d94bd4df96b51cb
nousinnihal009 <nousinnihal2004@gmail.com>
2026-04-12 20:23:31 +0530

    Create variables.tf

---
+++ infra/terraform/modules/gke/variables.tf
@@ -0,0 +1,33 @@
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "zone" {
+  type = string
+}
+variable "app_name" {
+  type = string
+}
+variable "node_count" {
+  type = number
+}
+variable "machine_type" {
+  type = string
+}
+variable "gpu_type" {
+  type = string
+}
+variable "gpu_count" {
+  type = number
+}
+variable "network_id" {
+  type = string
+}
+variable "subnetwork_id" {
+  type = string
+}
+variable "sa_email" {
+  type = string
+}
