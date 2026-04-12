d0474412557b812b6f4fb9b41314734ea3f81c8b
nousinnihal009 <nousinnihal2004@gmail.com>
2026-04-12 20:00:18 +0530

    Create variables.tf

---
+++ infra/terraform/modules/cache/variables.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "app_name" {
+  type = string
+}
+variable "redis_tier" {
+  type = string
+}
+variable "redis_memory_gb" {
+  type = number
+}
+variable "network_id" {
+  type = string
+}
